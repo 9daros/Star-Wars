e277e92f7ecbd047362c09b3ea5bee82fd39371c
9daros <151584703+9daros@users.noreply.github.com>
2024-09-08 22:36:11 -0300

    Update AboutScreen.jsx

--- src/screens/AboutScreen.jsx
+++ src/screens/AboutScreen.jsx
@@ -4,13 +4,9 @@ import { View, Text, StyleSheet } from 'react-native';
 const AboutScreen = () => {
   return (
     <View style={styles.container}>
-      <Text>RA:1134697 </Text>
-      <Text>Nome Completo: Ricardo de Assunção Dahmer</Text>
-      <Text>Email: 1134697@atitus.edu.br</Text>
-      <Text>------------------------------------------------------------------------------</Text>
-      <Text>RA:1135584 </Text>
-      <Text>Nome Completo: Roberto Jacobs</Text>
-      <Text>Email: 1135584@atitus.edu.br</Text>
+      <Text>RA:1135569 </Text>
+      <Text>Nome Completo: Henrique Daros</Text>
+      <Text>Email: 1135569@atitus.edu.br</Text> 
     </View>
   );
 };
